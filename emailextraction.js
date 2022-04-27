957005440f6deddb659a3759369d74e2a0eb8990
hbishop1996@gmail.com <Hannah.Bishop@softwire.com>
2022-04-27 12:42:05 +0100

    part 3 first draft

--- emailextraction.js
+++ emailextraction.js
@@ -1,5 +1,30 @@
 const fs = require('fs')
 const file = fs.readFileSync('test.txt','utf8')
-const regEx = /(@softwire\.com)\b/g
+
+
+const regEx = /\w+@\w+(\.\w+)+\b/g
+const regEx2 = /@\w+(\.\w+)+\b/g
 const matches = file.match(regEx)
-console.log(matches.length)
+const matchesDomains = file.match(regEx2)
+
+const domains = {}
+
+for (let i=0; i< matchesDomains.length; i++){
+    if (domains[matchesDomains[i]] === undefined){
+        domains[matchesDomains[i]]=1
+    } else {
+        domains[matchesDomains[i]] ++
+    }
+}
+console.log(domains)
+
+let frequencyCount = 0
+let mostFreqDom = ''
+for (let domainName in domains){
+    if (domains[domainName] > frequencyCount){
+        frequencyCount = domains[domainName]
+        mostFreqDom = domainName
+    }
+}
+
+console.log(`The most common domain is ${mostFreqDom} with a frequency of ${frequencyCount}`)
