4a9b39b8891cf222737f3a916e3c42a6e5b885f0
hbishop1996@gmail.com <Hannah.Bishop@softwire.com>
2022-04-27 13:55:05 +0100

    stretch goal 3

--- emailextraction.js
+++ emailextraction.js
@@ -1,7 +1,6 @@
 const fs = require('fs')
 const file = fs.readFileSync('test.txt','utf8')
-
-
+//parts 2 and 3
 const regEx = /\w+@\w+(\.\w+)+\b/g
 const regEx2 = /@\w+(\.\w+)+\b/g
 const matches = file.match(regEx)
@@ -16,7 +15,7 @@ for (let i=0; i< matchesDomains.length; i++){
         domains[matchesDomains[i]] ++
     }
 }
-
+console.log(matchesDomains.length)
 
 let domains_array = []
 for (let domainName in domains){
@@ -26,9 +25,17 @@ for (let domainName in domains){
 domains_array.sort((a, b) => b[0] - a[0])
 console.log(`The most common domain is ${domains_array[0][1]} with a frequency of ${domains_array[0][0]}`)
 
-
+//Stretch Goal 1
 console.log('The 10 most common domains and their frequeny are:')
 for (let j=0; j<10; j++){
     console.log(domains_array[j][1], domains_array[j][0])
 }
 
+//Stretch Goal 2
+desiredFrequency = 63
+console.log(`The domains that occur ${desiredFrequency} times or over are: `)
+for (let k=0; k<domains_array.length; k++){
+    if (domains_array[k][0] >= desiredFrequency){
+        console.log(domains_array[k][1])
+    }
+}
