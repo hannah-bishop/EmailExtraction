995537f811591f34f71f3aef6da33bd87e8903ca
hbishop1996@gmail.com <Hannah.Bishop@softwire.com>
2022-04-27 11:31:21 +0100

    fiding emails crudely

--- emailextraction.js
+++ emailextraction.js
@@ -1,4 +1,13 @@
 const fs = require('fs')
 const file = fs.readFileSync('test.txt','utf8')
+words_arr = file.split(/\s+/)
 
-console.log(file)
+let counter = 0
+for (let i=0; i<words_arr.length; i++){
+    if (words_arr[i].endsWith('@softwire.com')){
+    console.log(words_arr[i])
+    counter++
+    }
+}
+
+console.log(counter)
